347bcb23fe3bcb90625988eb40b0b2b5bc9e261c
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-11-02 19:05:58 +0800

    Implemented all file operations

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-//
-//  main.cpp
-//  movieAssign
-//
-//  Created by Apple on 10/29/24.
-//
+// [✓] Implement Struct for movies
+// TODO: Implement file read and write + debug print
+// TODO: Arrays and pointers movies
+// TODO: Implement class for movie management
+// TODO: Recursion for calculating average ratings
+// TODO: Attractive GUI using string
+// TODO: Report (cover page, table of content, intro about topic, explain important codes, example output)
+// ** Program must contain string input/output, struct, class, array, pointer, recursion, file operations 
 
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 struct Movie
@@ -15,6 +19,8 @@ struct Movie
     int rating;
 };
 
+void loadMovies(Movie* movies);
+void saveMovies(Movie* movies);
 
 int main()
 {
@@ -71,4 +77,49 @@ int main()
     
     
     return 0;
+}
+
+void loadMovies(Movie* movies)
+{
+  ifstream inFile("movies.txt");
+
+  if (!inFile)
+  {
+    cout << "Load Movies failed." << endl;
+    return;
+  }
+
+  string line;
+  getline(inFile, line);
+  int cur = 0;
+  while (getline(inFile, line))
+  {
+    int i = 0;
+    int temp = line.find("|");
+    while(i != temp) movies[cur].title += line[i++];
+    i++;
+    temp = line.find("|", i);
+    while(i != temp) movies[cur].releaseYear += line[i++];
+    i++;
+    while(i < line.length()) movies[cur].ratings += line[i++];
+  }
+}
+
+void saveMovies(Movie* movies)
+{
+  ofstream outFile("movies.txt");
+
+  if (!outFile)
+  {
+    cout << "Save Movies failed." << endl;
+    return;
+  }
+
+  cout << "Title|Release_Year|Ratings\n";
+
+  int cur = 0;
+  while(movies->title != "")
+  {
+    outFile << movies->title << "|" << movies->releaseYear << "|" << movies->ratings << "\n";
+  }
 }
